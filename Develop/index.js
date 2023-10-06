c198a8f7aa84d7062b56d76ef163d9c2db3dc74a
Luis Mansilla <l_mansilla92@yahoo.com>
2023-10-06 10:56:18 -0500

    added license choices

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // Creates an array of questions for user input
-const questions = ['What is the title of your project?', 'Enter a description of your project.', 'List the installation instructions for your project', 'Enter the usage information.', 'Enter the contribution guidelines', 'Enter test instructions.'];
+const questions = ['What is the title of your project?', 'Enter a description of your project.', 'List the installation instructions for your project', 'Enter the usage information.', 'Enter the contribution guidelines', 'Enter test instructions.', 'Please choose a license for your project.'];
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
@@ -50,20 +50,26 @@ inquirer
       name: 'linkedin',
       message: 'Enter your LinkedIn URL.',
     },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Please choose a license for your project.',
+      choices: ['Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'Mozilla Public License 2.0']
+    },
   ])
   // Stores the generateHTML content with the answers to the htmlPageContent constant
   .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+    const markDownPageContent = generateMarkDown(answers);
     // Uses fs.writeFile to write an index.html file with the content from htmlPageContent, and catches an error if there is one
-    fs.writeFile('index.html', htmlPageContent, (err) =>
+    fs.writeFile('README.md', markDownPageContent, (err) =>
     // Uses ternary operator to check if there's an error, if not then 'Successfully created index.js' is logged.
-      err ? console.log(err) : console.log('Successfully created index.html!')
+      err ? console.log(err) : console.log('Successfully created README.md!')
     );
   });
 
 // Generates HTML content that will be written to index.js by using template literals and accessing the data from the inquirer prompt objects
-const generateMarkDown = ({ name, location, github, linkedin, hobby, food }) =>
-  `# First Weather App
+const generateMarkDown = ({ title, description, installation, usage, contributions, test, github, linkedin}) =>
+  `# ${title}
 
   ## Repository
   
@@ -207,3 +213,21 @@ init();
 //       err ? console.log(err) : console.log('Successfully created index.html!')
 //     );
 //   });
+
+/*
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for information about my application repository
+THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+WHEN I enter my project title
+THEN this is displayed as the title of the README
+WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+WHEN I choose a license for my application from a list of options
+THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+WHEN I enter my GitHub username
+THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+WHEN I enter my email address
+THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+WHEN I click on the links in the Table of Contents
+THEN I am taken to the corresponding section of the README
+*/
